7119e606083ea648b4e605d6950eb0277effc477
islam-mac <islam-machine@gmail.com>
2021-07-13 09:58:17 +0100

    Started linear search code

---
+++ code/intro1/linearSearch.py
@@ -0,0 +1,12 @@
+# This will be it's own function
+
+def linear_search(search_list, target):
+    """
+
+    :param search_list:
+    :param target:
+    :return:
+    """
+    # A true implementation looks for the position in the list the target is
+    # position if in list (success), or none if failure or -1 since that is not typically index or exception. The failure return depends on needs
+    a = 2
